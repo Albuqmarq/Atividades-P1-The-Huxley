b3b2024404e0d1d156d5190003665510b195dcad
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:18:22 -0300

    Create 16.c

---
+++ Tarefas_1/16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int Nsorteado;
+    scanf("%d", &Nsorteado);
+    printf("O numero sorteado foi: %d\n", Nsorteado);
+    
+	return 0;
+}
