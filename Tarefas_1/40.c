2b1af37c2762fb6896841860ecffffbb9293b5de
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:19:14 -0300

    Create 40.c

---
+++ Tarefas_1/40.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    double valor, ValorFinal;
+    scanf("%lf", &valor);
+    ValorFinal = valor * 1.1;
+    printf("%.2lf\n", ValorFinal);
+        
+	return 0;
+}
