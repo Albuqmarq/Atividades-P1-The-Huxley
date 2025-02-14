bc54ad60f2f437df473e799b788b8445de721920
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:19:40 -0300

    Create 32.c

---
+++ Tarefas_1/32.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    double F, C;
+    scanf("%lf", &F);
+    C = (F-32)/1.8;
+    printf("%.2lf\n", C );
+    
+    
+	return 0;
+}
