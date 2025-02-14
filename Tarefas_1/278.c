115d8d2494b4244eeaf1bdc03c760edb5b7928d6
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:17:58 -0300

    Create 278.c

---
+++ Tarefas_1/278.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int valor1, valor2, valor3,valor4;
+    scanf("%d %d %d %d", &valor1, &valor2, &valor3, &valor4);
+    printf("DIFERENCA = %d\n", (valor1 * valor2)-(valor3 * valor4));
+    
+	return 0;
+}
