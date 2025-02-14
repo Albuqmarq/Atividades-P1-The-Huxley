65572edc9754ecee4d1be9f94184609a7ab31ad4
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:21:23 -0300

    Create 1042.c

---
+++ Tarefas_1/1042.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int idade, Isegundos;
+    scanf("%d", &idade);
+    Isegundos = idade * 31536000;
+    printf("%d\n", Isegundos);
+    
+    
+	return 0;
+}
