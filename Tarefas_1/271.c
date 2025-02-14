703d93dbbc57f8ca0af5e51bcf32a6244857d476
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:18:49 -0300

    Create 271.c

---
+++ Tarefas_1/271.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    double raio;
+    scanf("%lf", &raio);
+      double area;
+   
+   area = raio*raio*3.14159;
+  
+ 
+    printf("Area = %.4lf\n", area/10000);
+    
+    
+    
+    
+	return 0;
+}
