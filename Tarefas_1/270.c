df2dfcbc52be72141247537d6965fe53ff3a509b
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:20:20 -0300

    Create 270.c

---
+++ Tarefas_1/270.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int valor1, valor2;
+    scanf("%d %d",&valor1, &valor2);
+    printf("X = %d\n", valor1 +valor2);
+    
+    
+	return 0;
+}
