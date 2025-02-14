fada23357ea39bb3048bdf550980030ae3b3a6f1
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-13 23:21:55 -0300

    Create 22.c

---
+++ Tarefas_1/22.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+
+int a,i;
+scanf("%d", &a);
+
+for(i=1;i<10;++i){
+	printf("%d X %d = %d\n", a,i,a*i);
+}
+
+	return 0;
+}
