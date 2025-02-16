54d5f6703ebbbc5841d24009ec1723c83cc84c67
Marco Albuquerque <145617080+Albuqmarq@users.noreply.github.com>
2025-02-16 00:54:34 -0300

    Create 18.c

---
+++ Terefa 2/18.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int A,B,C;
+    scanf("%d %d %d", &A, &B, &C);
+    if (A<B && A<C){
+        printf("%d\n", A);
+    }
+    else if (B<A && B<C){
+        printf("%d\n", B);
+    }
+    else if( C<A && C<B){
+        printf("%d\n",C);
+    }
+    else if (A == B && A==C){
+        printf("%d\n",A);
+    }
+    else if (A==B && A<C){
+        printf("%d\n", A);
+    }
+    else if (A==C && A<C){
+        printf("%d\n",A);
+    }
+    else if (B==A && B<C){
+        printf("%d\n",B);
+    }
+    else if (B==C && B <A){
+        printf("%d\n", B);
+    }
+    else if (C==A && C<B){
+        printf("%d\n", C);
+    }
+    else if(C==B && C<A){
+        printf("%d\n",C);
+    }
+
+    
+	return 0;
+}
